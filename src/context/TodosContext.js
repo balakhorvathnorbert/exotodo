5ebfcedab13de99decf60d95cbb8f95502640e8f
Norbert Balak-Horvath <balakhorvath.norbert@icloud.com>
2023-01-19 13:22:40 +0100

    Defining context provider to access store data

---
+++ src/context/TodosContext.js
@@ -0,0 +1,14 @@
+import React, { createContext, useContext } from 'react';
+import { createTodoStore } from "../store/TodoStore";
+import { useLocalObservable } from 'mobx-react';
+
+const TodosContext = createContext(null)
+export const TodoProvider = ({ children }) => {
+  const todoStore = useLocalObservable(createTodoStore);
+
+  return (
+    <TodosContext.Provider value={todoStore}>{children}</TodosContext.Provider>
+  )
+}
+
+export const useTodoStore = () => useContext(TodosContext)
