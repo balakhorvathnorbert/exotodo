4d452911f330706ea4f12b69d9222009da47d860
Norbert Balak-Horvath <balakhorvath.norbert@icloud.com>
2023-01-23 11:16:53 +0100

    Remove log

--- src/Screens/AddEditTodoScreen.jsx
+++ src/Screens/AddEditTodoScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AddEditTodo from '../components/addedittodo/AddEditTodo';
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
